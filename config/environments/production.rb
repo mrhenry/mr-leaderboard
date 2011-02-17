832b29361058753eaab058cae90a3501a22f4f8f
yves <yvesvanbroekhoven@gmail.com>
2011-02-17 20:06:56 +0100

    Fixed subscribe url in mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ MrLeaderboard::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => "electric-sky-206.heroku.com" }
+  config.action_mailer.default_url_options = { :host => "mr-leaderboard.heroku.com" }
 end
