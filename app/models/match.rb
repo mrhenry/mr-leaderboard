1ea66555ea9fa12a5877f10c4ccd7845abdf1c75
yves <yvesvanbroekhoven@gmail.com>
2011-03-04 07:51:27 +0100

    Added membership_id add to score if not present yet, to make a transition so we can remove user_id

--- app/models/match.rb
+++ app/models/match.rb
@@ -12,7 +12,7 @@ class Match < ActiveRecord::Base
   def add_membership_statistics
     return if self.scores.count < 2
     self.scores.each do |score|
-      membership = Membership.first(:conditions => ["id = ?", score.membership_id])
+      membership = Membership.first(:conditions => ["id = ?", score.membership_id]) || find_membership_by_user_id(score, self.leaderboard_id)
       membership.played_games   += self.scores[0].score + self.scores[1].score
       membership.played_matches += 1
       membership.won_games      += score.score
@@ -33,5 +33,23 @@ class Match < ActiveRecord::Base
   end
   
 private
+  
+  def find_membership_by_user_id(score, leaderboard_id)
+    membership = Membership.first(:conditions => ["leaderboard_id = ? and user_id = ?", leaderboard_id, score.user_id])
+    
+    score.membership_id = membership.id
+    if score.update_attributes(score)
+      Rails.logger.debug('---------------')
+      Rails.logger.debug("saved")
+      Rails.logger.debug('---------------')
+    else
+      Rails.logger.debug('---------------')
+      Rails.logger.debug(errors.inspect)
+      Rails.logger.debug("not saved")
+      Rails.logger.debug('---------------')
+    end
+    
+    return membership
+  end
   
 end
