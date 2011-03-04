b180cf1586f9ad9ccf53088954d1ae27d36f0ef8
yves <yvesvanbroekhoven@gmail.com>
2011-03-04 07:13:10 +0100

    Redirect to leaderboard if second score is added

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -13,12 +13,12 @@ class ScoresController < ApplicationController
     
     if @score.save
       if @match.scores.count == 2
-        #send_match_confirmation
         notice = "Score added."
+        redirect_to leaderboard_url(params[:leaderboard_id]), :notice => "Score added"
       else
         notice = "Score added."
+        redirect_to leaderboard_match_url(params[:leaderboard_id], params[:match_id]), :notice => "Score added"
       end
-      redirect_to leaderboard_match_url(params[:leaderboard_id], params[:match_id]), :notice => notice
     else
       redirect_to leaderboard_match_url(params[:leaderboard_id], params[:match_id]), :notice => "Score not added because validation failure"
     end
@@ -26,13 +26,4 @@ class ScoresController < ApplicationController
 
 private
 
-  def send_match_confirmation
-    @match.scores.each do |score|
-      score_user = User.find(score.user_id)
-      unless score_user == current_user
-        #UserMailer.match_confirmation(score_user, @match).deliver
-      end
-    end
-  end
-  
 end
